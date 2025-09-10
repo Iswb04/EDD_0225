7b6d174853ad8df5c64863ccfbb04f4466e1539d
Iswb04 <145922868+Iswb04@users.noreply.github.com>
2025-09-10 11:33:55 -0300

    Update OrdenarNumeros.java

--- semana4/aulaordenacao03/controle/OrdenarNumeros.java
+++ semana4/aulaordenacao03/controle/OrdenarNumeros.java
@@ -1,7 +1,7 @@
 public class OrdenarNumeros {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // Ler números do usuário
-        int[] numeros = new int[10]; // Array para guardar 10 números
+        Scanner scanner = new Scanner(System.in); 
+        int[] numeros = new int[10];
 
         // Lê 10 números do usuário
         System.out.println("Digite 10 números inteiros:");
@@ -9,10 +9,10 @@ public class OrdenarNumeros {
             numeros[i] = scanner.nextInt();
         }
 
-        // Ordena os números usando Bubble Sort
+      
         for (int i = 0; i < numeros.length - 1; i++) {
             for (int j = 0; j < numeros.length - i - 1; j++) {
-                if (numeros[j] > numeros[j + 1]) { // Se estiver fora de ordem, troca
+                if (numeros[j] > numeros[j + 1]) { 
                     int temp = numeros[j];
                     numeros[j] = numeros[j + 1];
                     numeros[j + 1] = temp;
@@ -20,13 +20,13 @@ public class OrdenarNumeros {
             }
         }
 
-        // Mostra os números em ordem crescente
+       
         System.out.println("Números em ordem crescente:");
         for (int numero : numeros) {
             System.out.print(numero + " ");
         }
 
-        scanner.close(); // Fecha o scanner
+        scanner.close(); 
     }
 
       /*
